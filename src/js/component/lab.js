76d373829e2e2a84ee5c11e5bb087f10d84cf913
2cyprian <cyprianodindo@gmail.com>
2025-12-08 17:50:56 +0300

    changes home and skills card  and add some apdated designs    and ensure the website is responsive

--- src/js/component/lab.js
+++ src/js/component/lab.js
@@ -8,6 +8,23 @@ const teamMembers = [
     { name: "Maria", img: "/assets/tech/maria.svg" }
 ];
 
+const labsValues=[
+    {icon:"/assets/icons/mission.svg",
+        title:'Our Mission',
+        text:"To equip graduates and professionals with hands-on, industry-ready tech skills that bridge the gap between education and career opportunities."
+            },
+    {icon:"/assets/icons/vision.svg",
+        title:"Our Vision",
+        text:"To be Africa’s leading tech skills hub, empowering professionals to excel, innovate, and drive digital growth."
+    },
+    {
+        icon:"/assets/icons/values.svg",
+        title:"Our Values",
+        text:"We value hands-on learning, mentorship, inclusivity, industry relevance, and empowering learners to make a meaningful impact."
+
+    }
+]
+
 // --- RENDER TEAM MEMBERS ---
 
 // 1. Render Team
@@ -21,4 +38,19 @@ if (teamContainer) {
             <span class="member-name">${member.name}</span>
         </div>
     `).join('');
+}
+
+// 2.Render labValues
+
+const valuesContainer = document.getElementById('values-list');
+if (valuesContainer){
+    valuesContainer.innerHTML= labsValues.map(values => `
+        <div class="values-list">
+            <div class="values-icon">
+            <img src="${values.icon}" alt="values-icon">
+            </div>
+        <h3>${values.title}</h3>
+        <p>${values.text}</p>
+    
+        </div>`).join('');
 }
